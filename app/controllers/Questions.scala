5fbf8f645ca96ff35753e2cda959f129af94489d
Yura Susuk <yurasusuk@gmail.com>
2014-06-25 16:35:12 +0200

    all 71 qeustions were added

--- app/controllers/Questions.scala
+++ app/controllers/Questions.scala
@@ -12,23 +12,20 @@ case class Question(id: String, text: String, answer: Boolean)
 
 object Questions extends Controller with MongoController {
 
-  def collection: JSONCollection = db.collection[JSONCollection]("questions")
-
-  //  def list = Action.async {
-  //    collection
-  //  }
 
   implicit val questionWrites: Writes[Question] = (
-     (JsPath \ "id" ).write[String] and
+    (JsPath \ "id").write[String] and
       (JsPath \ "text").write[String] and
       (JsPath \ "answer").write[Boolean]
     )(unlift(Question.unapply))
 
   implicit val questionReads: Reads[Question] = (
-    (JsPath \ "_id" \ "$oid" ).read[String] and
+    (JsPath \ "_id" \ "$oid").read[String] and
       (JsPath \ "text").read[String] and
       (JsPath \ "answer").read[Boolean]
     )(Question.apply _)
+
+  def collection: JSONCollection = db.collection[JSONCollection]("questions")
 
   def list = Action.async {
     val cursor: Cursor[Question] = collection.find(Json.obj()).cursor[Question]
