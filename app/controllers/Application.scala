6fb8cd8e0438df0e8f49530267a20b445799a990
ysusuk <yurasusuk@gmail.com>
2014-06-27 17:25:40 +0200

    socialsecure was tuned a bit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 
 object Application extends Controller with securesocial.core.SecureSocial {
 
-  def index = SecuredAction {
+  def index = Action {
     Ok(views.html.main.render)
   }
 }
---
+++ app/controllers/UserService.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import play.api.Application
+import securesocial.core.{Identity, IdentityId, UserServicePlugin}
+import securesocial.core.providers.Token
+
+class UserService(application: Application) extends UserServicePlugin(application) {
+  var users: Map[IdentityId, Identity] = Map.empty
+
+  override def find(id: IdentityId): Option[Identity] = users get id
+
+  override def findByEmailAndProvider(email: String, providerId: String): Option[Identity] = None
+
+  override def deleteToken(uuid: String): Unit = Unit
+
+  override def save(user: Identity): Identity = {
+    users = users + (user.identityId -> user)
+    user
+  }
+
+  override def save(token: Token): Unit = Unit
+
+  override def findToken(token: String): Option[Token] = None
+
+  override def deleteExpiredTokens(): Unit = Unit
+}
